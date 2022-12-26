2d7279b97c0ce65d1b579f28fc2e668293a15555
kyozun <115677282+kyozun@users.noreply.github.com>
2022-12-26 10:00:06 +0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SlideBarComponent } from './pages/slide-bar/slide-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideBarComponent
   ],
   imports: [
     BrowserModule
