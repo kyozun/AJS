b73dfac89ed6f6de3774fc7c5cc771cb861552c8
kyozun <115677282+kyozun@users.noreply.github.com>
2022-12-26 10:25:15 +0700

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SlideBarComponent } from './pages/slide-bar/slide-bar.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SlideBarComponent } from './pages/slide-bar/slide-bar.component';
     SlideBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
